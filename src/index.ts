1237b9a6415ba7b4510b556a22dc32a903e9155c
yaoxfly <1315892088@qq.com>
2022-11-11 22:24:42 +0800

    feat: 添加备份还原命令

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 #!/usr/bin/env node
-import {upload} from './upload'
+import { upload } from './upload'
 import { Command } from 'commander'
+import { backup } from './backup'
 const program = new Command()
 
 program
   .version(require('../version').version)
   .name('yx-deploy')
 
-  program
-  .command('upload' ).alias('u')
+program
+  .command('upload').alias('u')
   .description('Upload project')
   .action(() => {
     upload()
+  })
+
+program
+  .command('revert').alias('r')
+  .description('Revert project')
+  .action(() => {
+    backup()
   })
 
 program.parse(process.argv)
