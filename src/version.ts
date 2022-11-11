e0a828314813428f9d3604ab2b4dd3152d2f7a72
yaoxfly <1315892088@qq.com>
2022-11-11 22:23:03 +0800

    style：代码格式修改

--- src/version.ts
+++ src/version.ts
@@ -4,9 +4,7 @@ import fs from 'fs'
 import chalk from 'chalk'
 import symbols from 'log-symbols';
 const packageJson = readFile(resolve('../package.json'))
-fs.writeFile(resolve('../version.json'), JSON.stringify({version:packageJson.version}, null, 2), function (err) {
+fs.writeFile(resolve('../version.json'), JSON.stringify({ version: packageJson.version }, null, 2), function (err) {
   if (err) console.error(err)
   console.log(symbols.success, chalk.green('version.json文件已经更新'));
 })
-
-
