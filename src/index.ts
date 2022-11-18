fec0c9d5a27b496caf7c668bf7e4ce26d51af7f2
yaoxfly <1315892088@qq.com>
2022-11-18 12:01:03 +0800

    feat：添加多环境命令

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,28 @@
 import { upload } from './upload'
 import { Command } from 'commander'
 import { backup } from './backup'
-const program = new Command()
+import {useConfig} from './config' 
 
+const program = new Command()
 program
   .version(require('../version').version)
   .name('yx-deploy')
 
 program
   .command('upload').alias('u')
+  .option('-e --env <env-name>', 'Please enter an environment')
   .description('Upload project')
-  .action(() => {
-    upload()
+  .action((option) => {
+    upload(option,useConfig)
   })
 
 program
   .command('revert').alias('r')
   .description('Revert project')
   .option('-l --latest', 'Revert latest version')
+  .option('-e --env <env-name>', 'Please enter an environment')
   .action((options:any) => {
-    backup(options)
+    backup(options,useConfig)
   })
 
 program.parse(process.argv)
